10ff2f56ce2e77f71b26fb3ceab739e54497ef34
Jacob <jacob.pancoast17@gmail.com>
2026-06-30 16:22:57 -0400

    Add recharts, test SIR graphs in api.py and show a frontend slider

--- backend/app/api.py
+++ backend/app/api.py
@@ -1,5 +1,8 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
+import matplotlib.pyplot as plt
+import numpy as np
+from scipy.integrate import odeint
 
 app = FastAPI()
 
@@ -19,3 +22,35 @@ app.add_middleware(
 @app.get("/", tags = ["root"])
 async def read_root() -> dict:
     return {"message" : "Welcome to the website!"}
+
+@app.get("/api/outbreakr", tags = ["outbreakr"])
+async def show_outbreakr() -> dict:
+    return {"outbreaks" : "wip"}
+
+def dAdt(A, T, beta, gamma, N):
+    S = A[0]
+    I = A[1]
+    R = A[2]
+
+    return [
+        -beta / N * I * S,
+        beta / N * I * S - gamma * I,
+        gamma * I
+    ]
+
+times = np.arange(0, 100, 1)
+beta = 1/2
+gamma = 1/5
+N = 1000
+S0, I0, R0 = N - 6, 6, 0
+
+solved = odeint(dAdt, y0 = [S0, I0, R0], t = times, args = (beta, gamma, N))
+solved_transposed = solved.transpose()
+
+plt.plot(times, solved_transposed[0], label = "Susceptible")
+plt.plot(times, solved_transposed[1], label = "Infected")
+plt.plot(times, solved_transposed[2], label = "Removed")
+
+plt.legend()
+
+plt.show()
--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,19 @@
+import { useState } from 'react'
+
 function App() {
+  const [count, setCount] = useState(0)
 
   return (
     <main className="py-10">
       <h1 className = "font-bold text-3xl text-center">Outbreakr</h1>
+      <div>
+        <label>
+          <input type="range" min="0" max="5" step="1" value={count} onChange={(e) => setCount(Number(e.currentTarget.value))}/>
+          <span>
+            {count}
+          </span>
+        </label>
+      </div>
     </main>
   )
 }
