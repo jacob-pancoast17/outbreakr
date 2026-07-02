7b8e1264c31cd5ef002cd97bb9ea6a467f9c627f
Jacob <jacob.pancoast17@gmail.com>
2026-07-01 20:53:41 -0400

    add graph framework

--- frontend/src/components/Graph.tsx
+++ frontend/src/components/Graph.tsx
@@ -1,2 +1,17 @@
+import { React } from 'react';
+import { CartesianGrid, Line, LineChart, XAxis, YAxis, Legend } from 'recharts';
+import { dummyData } from '../data/sample';
 
+export default function Chart() {
+    return (
+        <LineChart style={{ width: '100%', aspectRatio: 1.618, maxWidth: 800, margin: 'auto'}} responsive data={dummyData}>
+            <CartesianGrid strokeDasharray="5 5" />
+            <XAxis dataKey="name" />
+            <YAxis width="auto" />
+            <Legend />
+            <Line type="monotone" dataKey="susceptible" stroke=""/>
+                
+        </LineChart>
+    )
+}
 
---
+++ frontend/src/data/sample.ts
@@ -0,0 +1,12 @@
+import { Day } from "../types/day";
+
+export const dummyData: Day[] = [
+    {
+        day: 0,
+        susceptible: 10
+    },
+    {
+        day: 1,
+        susceptible: 8
+    },
+];
